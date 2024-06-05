6ab1f0929aee202646877e96e1c0f43f0ae21885
EsVagy <sltamagotchi@gmail.com>
2024-06-05 07:04:20 +0000

    Added update_sim

--- hourglass_new.ino
+++ hourglass_new.ino
@@ -9,15 +9,15 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 void setup() {
   // put your setup code here, to run once:
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  display.clearDisplay();
+  display.drawPixel(10, 10, true);
+  update_sim(&display);
   
-
-
   display.display();
 
   delay(10);
@@ -45,6 +45,14 @@ void update_sand(Adafruit_SSD1306 *display, int x, int y) {
       display.drawPixel(nx, ny, true);
       display.drawPixel(x, y, false);
       return;
+    }
+  }
+}
+
+void update_sim(Adafruit_SSD1306 *display) {
+  for (int y = display.height() - 1; y >= 0; y--) {
+    for (int x = display.width() - 1; x >= 0; x--) {
+      update_sand(display, x, y);
     }
   }
 }
