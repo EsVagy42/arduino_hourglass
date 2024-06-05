1b438ccd5f3219016516b6dc8255dbf991ef17d5
EsVagy <sltamagotchi@gmail.com>
2024-06-05 06:47:15 +0000

    Added is_valid function

--- hourglass_new.ino
+++ hourglass_new.ino
@@ -1,9 +1,31 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  display.clearDisplay();
+  
+
+
+  display.display();
+
+  delay(10);
+}
+
+void is_valid(Adafruit_SSD1306 *display, int x, int y) {
+  if (x < 0 || x >= display->width() || y < 0 || y >= display->height()) {
+    return false;
+  }
+  return true;
 }
