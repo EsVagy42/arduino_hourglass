a9fdc2a607b18dc98a06d6bb166bced72e27fe77
EsVagy <sltamagotchi@gmail.com>
2024-06-07 12:34:20 +0000

    Added different fall rules for different orientations to sand

--- hourglass_new.ino
+++ hourglass_new.ino
@@ -57,7 +57,7 @@ void setup() {
 }
 
 void loop() {
-  update_sim(&display, &timer);
+  update_sim(&display, &timer, 0);
   draw_remaining_time(&display, &timer);
   display.display();
 
@@ -94,14 +94,14 @@ void fill_hourglass(Adafruit_SSD1306 *display, int sand_count) {
   }
 }
 
-void update_sand(Adafruit_SSD1306 *display, int x, int y) {
-  static int neighs[3][2] = {{0, 1}, {-1, 1}, {1, 1}};
+void update_sand(Adafruit_SSD1306 *display, int x, int y, int rotation) {
+  static int neighs[4][3][2] = {{{0, 1}, {-1, 1}, {1, 1}}, {{1, 0}, {1, 1}, {1, -1}}, {{0, -1}, {1, -1}, {-1, -1}}, {{-1, 0}, {-1, -1}, {-1, 1}}};
 
   if (!is_valid(display, x, y) || !display->getPixel(x, y)) {
     return;
   }
   for (int i = 0; i < 3; i++) {
-    int nx = x + neighs[i][0], ny = y + neighs[i][1];
+    int nx = x + neighs[rotation][i][0], ny = y + neighs[rotation][i][1];
     if (!is_valid(display, nx, ny)) {
       continue;
     }
@@ -113,12 +113,12 @@ void update_sand(Adafruit_SSD1306 *display, int x, int y) {
   }
 }
 
-void update_sim(Adafruit_SSD1306 *display, Timer *timer) {
+void update_sim(Adafruit_SSD1306 *display, Timer *timer, int rotation) {
   for (int y = display->height() - 1; y >= 0; y--) {
     for (int x = display->width() - 1; x >= 0; x--) {
       if (!(x == opening_pos[0] && y == opening_pos[1]) ||
           is_opening_open(display, timer)) {
-        update_sand(display, x, y);
+        update_sand(display, x, y, rotation);
       }
     }
   }
